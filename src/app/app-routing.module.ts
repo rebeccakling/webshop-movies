bcb90dab002f0d84c98a1ec7639e078510472445
RebeccaKling <kling.rebecca@hotmail.com>
2019-05-02 14:38:54 +0200

    Add new components and print out the movies

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ShowmoviesComponent } from './showmovies/showmovies.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //Detta blir min nya 'index' sida.
+  { path: '', component: ShowmoviesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
