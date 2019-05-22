08a5029e30dbe49f383a857f46371a51ffabac40
RebeccaKling <kling.rebecca@hotmail.com>
2019-05-22 13:32:48 +0200

    Added test for  one movie and revove from cart

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -29,14 +29,14 @@ describe('HeaderComponent', () => {
     expect(component.cart.length).toEqual(0);
 
     const service = new MockDataService();
- 
+
     service.fetchMovies().subscribe((movies) => {
       component.addToCart(movies[0]);
- 
+
       expect(component.cart.length).toEqual(1);
     });
   });
-  
+
   it('should add two diffrent movie', () => {
     const service = new MockDataService();
 
@@ -51,7 +51,7 @@ describe('HeaderComponent', () => {
 
   it('should add two duplicate movies and increase amount to 2', () => {
     const service = new MockDataService();
- 
+
     service.fetchMovies().subscribe((movies) => {
 
       component.addToCart(movies[0]);
@@ -61,5 +61,43 @@ describe('HeaderComponent', () => {
     });
   });
 
-  
+  it('should add one movie to cart', () => {
+    const service = new MockDataService();
+
+    service.fetchMovies().subscribe((movies) => {
+
+      component.addToCart(movies[1]);
+      component.addOneMoreMovie(76);
+
+      expect(component.cart.length).toEqual(1);
+    });
+  });
+
+
+  it('should remove one movie from cart', () => {
+    const service = new MockDataService();
+
+    service.fetchMovies().subscribe((movies) => {
+
+      component.addToCart(movies[0]);
+      component.addOneMoreMovie(76);
+
+      expect(component.cart.length).toEqual(1);
+    });
+  });
+
+  // it('should count the totalprice in cart', () => {
+  //   const service = new MockDataService();
+
+  //   service.fetchMovies().subscribe((movies) => {
+
+  //     component.addToCart(movies[0]);
+  //     component.addOneMoreMovie(76);
+  //     component.countTotalPrice();
+
+  //     expect(component.cart.length).toEqual(1);
+  //   });
+  // });
+
+
 });
